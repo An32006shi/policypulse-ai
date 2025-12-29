878559afdedd8aa7df520e982bdc2bd5cdb447a7
Anshika Gupta <yg1093911@gmail.com>
2025-12-29 12:22:14 +0530

    Fix Vercel fullstack deployment

--- server/index.js
+++ server/index.js
@@ -40,6 +40,10 @@ app.get('/', (req, res) => {
     res.send('PolicyPulse API is running');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+export default app;
